1ebfc08482ed8627f28328009db561074efef8c3
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-07-03 17:21:15 -0400

    Create 2 Buckets + Bug Fixes

--- buckets/buckets.tf
+++ buckets/buckets.tf
@@ -1,28 +1,25 @@
 #---------buckets/buckets.tf-----------
 # Build 2 different buckets: #1 for the Raw Data, #2 for 'dataflow'
-# variable "bucket_prefix" {
-#   description = "Prefix for unique bucket names"
-# }
 
 resource "google_storage_bucket" "raw_data_bucket" {
-  name          = "raw-data-${var.bucket_prefix}"
+  name          = "raw-data-${var.bucket_prefix}-de"
   force_destroy = false
   location      = "US"
   storage_class = "STANDARD"
 
-  labels = var.default_label
+  # labels = var.default_label
   versioning {
     enabled = true
   }
 }
 
 resource "google_storage_bucket" "data_flow_bucket" {
-  name          = "data-flow-${var.bucket_prefix}"
+  name          = "data-flow-${var.bucket_prefix}-de"
   force_destroy = false
   location      = "US"
   storage_class = "STANDARD"
 
-  labels = var.default_label
+  # labels = var.default_label
   versioning {
     enabled = true
   }
