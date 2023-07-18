40aa5b40b2b9db2e64a93ab1a023ffc86c74cbb6
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-07-17 20:40:02 -0400

    Create SYS cli args, logger + initial parsing

--- data-transforms-python/temperature-transform.py
+++ data-transforms-python/temperature-transform.py
@@ -1,12 +1,98 @@
 # Cleans up and transforms the raw temperature tables into a CSV file for BigQuery
 import logging
 import csv
+import sys, getopt
+import os
 from datetime import datetime
 
-# Create a SYS argument function that will take the input file name and output file name arguments in the cli
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+# This the function where we want to entry the script
+"""
+To execute this script, input this on the CLI from the current directory where the script is located:
+    python temperature-transform.py -i <input_file.csv> -o <output_file.csv>
+"""
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
     
+    try:
+       opts, args = getopt.getopt(argv[1:],"hi:o:c:",["ifile=","ofile="])
+    except getopt.GetoptError:
+       print (f"{argv[0]} -i <inputfile> -o <outputfile> ")
+       sys.exit(2)
+    for opt, arg in opts:
+       if opt == '-h':
+           print (f"{argv[0]} -i <inputfile> -o <outputfile> ")
+           sys.exit()
+       elif opt in ("-i", "--ifile"):
+           # get the location of this script
+            script_directory = os.path.dirname(os.path.abspath(__file__))
+            # build the full path to the input file location
+            inputfile = os.path.join(os.path.dirname(script_directory), "raw-files", arg)
+
+       elif opt in ("-o", "--ofile"):
+           # get the path of the script's directory and send the 'outfile' to the original 'raw-files' directory
+           script_directory = os.path.dirname(os.path.abspath(__file__))
+           outputfile = os.path.join(os.path.dirname(script_directory), "raw-files", arg)
+       
+
+    logger.info('Input file is: ' + str(inputfile))
+    parse_raw_csv(inputfile, outputfile)
+    logger.info('Output file is: ' + str(outputfile))
+
+def parse_raw_csv(input_file_name, output_file_name):
+    # Initialize empty lists for each column
+    station_id = [] # string type
+    date = [] #integer type
+    weather_element = [] # string type
+    weather_el_data_val = [] # integer type
+    measurement_flag = [] # string type
+    quality_flag = [] # string type
+    source_flag = [] # string type
+    obs_time = [] #integer:  4-character time of observation in hour-minute format (i.e. 0700 =7:00 am)
+
+
+    with open(input_file_name, 'r') as infile:
+        csv_reader = csv.reader(infile)
+        next(csv_reader)
+        for row in csv_reader:
+            # Append each value to the respective column list
+            station_id.append(row[0])
+            date.append(int(row[1]))
+            weather_element.append(row[2])
+            weather_el_data_val.append(int(row[3]))
+            measurement_flag.append(row[4])
+            quality_flag.append(row[5])
+            # source_flag.append(row[6]) # is the source flag for the first day of the month - I don't need this original column
+            # obs_time.append(int(row[7])) - I don't need this original column
+
+   # Perform desired transformations on the lists (e.g., lambda functions)
+
+    # Print the transformed lists
+    print("string_field_0:", station_id)
+    # print("int64_field_1:", int64_field_1)
+    # print("string_field_2:", string_field_2)
+    # print("int64_field_3:", int64_field_3)
+    # print("string_field_4:", string_field_4)
+    # print("string_field_5:", string_field_5)
+    # print("string_field_6:", string_field_6)
+    # print("int64_field_7:", int64_field_7)
+    with open(output_file_name, 'w', newline='') as outfile:
+        csv_writer = csv.writer(outfile)
+        # Outfile headers:
+        # csv.writer.writerow(["station_id", "date", "wx_elm", "wx_elm_dt_val", "mmt_flag", "q_flag"])
+        for i in range(len(station_id)):
+            csv_writer.writerow([station_id[i],date[1]])
+
+
 def get_usa_stations():
     pass
 
 def convert_dates():
     pass
+
+if __name__ == '__main__':
+  main(sys.argv)
+  logging.getLogger().setLevel(logging.INFO)
